f22ef74c2d55656781673573517f412083184ba7
Nachiket <84851340+Nachiket16@users.noreply.github.com>
2023-05-17 11:48:08 +0530

    Update GreetingController.java

--- config client/config-client-first/src/main/java/com/nachiket/config/clientfirst/GreetingController.java
+++ config client/config-client-first/src/main/java/com/nachiket/config/clientfirst/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class GreetingController {
   @Value("${docs.uri}")
   private String url;
